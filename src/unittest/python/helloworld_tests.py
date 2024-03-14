cc7bd90718fdaada30e68d4fd0c3b3bd8587e39b
bharathkondam <bharath.kondam@gmail.com>
2024-03-14 17:30:35 +0530

    firstpythonUnitTestProject

--- src/unittest/python/helloworld_tests.py
+++ src/unittest/python/helloworld_tests.py
@@ -7,7 +7,6 @@ from helloworld import helloworld
 class HelloWorldTest(unittest.TestCase):
     def test_should_issue_hello_world_message(self):
         out = mock()
-        
         helloworld(out)
         
         verify(out).write("Hello world of Python\n")
